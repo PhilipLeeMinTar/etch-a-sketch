c2785e2a0b266e9ed8f858925ebcaeea5cf5a44e
PhilipLeeMinTar <philiplee98@gmail.com>
2023-05-10 00:27:54 +0800

    done

--- source.js
+++ source.js
@@ -1,29 +1,36 @@
 const btn = document.querySelector('button')
-
 const container = document.querySelector('.container');
 
-for (let i=0;i<16;i++) {
-    const column = document.createElement('div');
-    for (let i=0;i<16;i++){
-        const square_div = document.createElement('div');
-        square_div.setAttribute('style', 'width: 50px; height: 50px; margin: 10px;');
-        // square_div.style.backgroundColor = 'blue';
-        square_div.addEventListener('mouseover', (event) => {
-            event.target.style.backgroundColor = "orange";
-        });
-        
-        square_div.addEventListener('mouseout', (event) => {
-            event.target.style.backgroundColor = "";
-        });
-        column.appendChild(square_div);
+const createGrid = (size) => {
+    for (let i=0;i<size;i++) {
+        const column = document.createElement('div');
+        for (let i=0;i<size;i++){
+            const square_div = document.createElement('div');
+            square_div.setAttribute('style', 'width: 9.5px; height:9.5px;');
+            square_div.style.backgroundColor = 'blue';
+            square_div.addEventListener('mouseover', (event) => {
+                event.target.style.backgroundColor = "orange";
+            });
+            
+            square_div.addEventListener('mouseout', (event) => {
+                event.target.style.backgroundColor = "blue";
+            });
+            column.appendChild(square_div);
+        }
+        container.appendChild(column);
     }
-    container.appendChild(column);
 }
+
+createGrid(16);
 
 btn.addEventListener('click', () => {
     let side = parseInt(prompt("Put a number less than 100"));
-})
+    if (side > 100) {
+        alert("Number is not less than 100");
+        return;
+    } else {
+        container.textContent = "";
+        createGrid(side);
+    }
 
-console.log("hihi")
-// container.textContent = "what is this";
-console.log("ajkgej")
+})
